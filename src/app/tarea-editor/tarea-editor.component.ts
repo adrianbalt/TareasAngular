83f5b583dc8002ed4a2575580033214d7c4c3878
Adrian Baltazar <adrian@baltazar.pe>
2019-10-31 09:05:47 -0500

    se usaron template references para agregar focus al editor de tareas y no se pueden usar datos de estado para componentes dinamicos :(

--- src/app/tarea-editor/tarea-editor.component.ts
+++ src/app/tarea-editor/tarea-editor.component.ts
@@ -1,32 +1,46 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Tarea} from '../tarea'
 import {FormControl} from '@angular/forms'
+import {ViewChild, AfterViewInit, ElementRef} from '@angular/core'
 
 @Component({
   selector: 'app-tarea-editor',
   templateUrl: './tarea-editor.component.html',
   styleUrls: ['./tarea-editor.component.styl']
 })
-export class TareaEditorComponent implements OnInit {
+export class TareaEditorComponent implements OnInit, AfterViewInit {
 
 	@Input() tarea: Tarea;
 
+  nameTarea: String;
+  completado: boolean;
 	editando: boolean = false;
+
+  @ViewChild("inputNameTarea",{static:false}) inputNameTarea: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.nameTarea = this.tarea.name;
+    this.completado = this.tarea.completado;
   }
 
+  ngAfterViewInit(){}
+
   onDblClick(){
-  	console.log("aaaa")
-  	this.editando = !this.editando
+  	this.editando = true
+    setTimeout(()=>{
+      this.inputNameTarea.nativeElement.focus()
+    },10)
   }
 
   onBlur(){
-  	console.log("eidtorrrr")
-  	// console.log(editorForm)
   	this.editando = false
+  }
+
+  onSubmit(){
+    console.log(this.tarea.name)
+    this.editando = false;
   }
 
 }
